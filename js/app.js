b5c376dc0a2364803dbb9181bba4751e2c7a8bed
Diego Orellana <dorellanaolivo@gmail.com>
2018-03-25 22:11:42 -0500

    Se Crea la funcion cuando se pulsa una tecla, se agregan las validaciones de la calculadora

---
+++ js/app.js
@@ -0,0 +1,59 @@
+var Calculadora = {
+
+  tecla: document.getElementsByClassName('tecla'),
+  display : document.getElementById('display'),
+
+  pulsa: function (tecla)
+  {
+    tecla.style.padding = '2px';
+    setTimeout(function () {tecla.style.padding = '0px';}, 50);
+
+    if (!isNaN(tecla.alt) && display.textContent.length < 8) {
+      if (display.textContent.length == 1 && display.textContent == "0")
+        display.textContent = tecla.alt;
+      else
+        display.textContent += tecla.alt;
+    }
+    else if (tecla.alt == "punto" && display.textContent.length < 8 && display.textContent.indexOf(".") == -1) {
+      display.textContent += "."
+    }
+
+    if (tecla.alt == "On") {
+      display.textContent = "0";
+    }
+
+    if (tecla.alt == "signo" && display.textContent != "0") {
+        if (display.textContent.indexOf("-") == -1)
+          display.textContent = "-" + display.textContent;
+        else
+          display.textContent = display.textContent.replace("-","")
+    }
+
+
+
+  },
+
+  inicializar: function () {
+    this.tecla[0].onclick = function(){ Calculadora.pulsa(Calculadora.tecla[0])}//ON/C
+    this.tecla[1].onclick = function(){ Calculadora.pulsa(Calculadora.tecla[1])}//+/-
+    this.tecla[2].onclick = function(){ Calculadora.pulsa(Calculadora.tecla[2])}//raiz
+    this.tecla[3].onclick = function(){ Calculadora.pulsa(Calculadora.tecla[3])}//div
+    this.tecla[4].onclick = function(){ Calculadora.pulsa(Calculadora.tecla[4])}//7
+    this.tecla[5].onclick = function(){ Calculadora.pulsa(Calculadora.tecla[5])}//8
+    this.tecla[6].onclick = function(){ Calculadora.pulsa(Calculadora.tecla[6])}//9
+    this.tecla[7].onclick = function(){ Calculadora.pulsa(Calculadora.tecla[7])}//x
+    this.tecla[8].onclick = function(){ Calculadora.pulsa(Calculadora.tecla[8])}//4
+    this.tecla[9].onclick = function(){ Calculadora.pulsa(Calculadora.tecla[9])}//5
+    this.tecla[10].onclick = function(){ Calculadora.pulsa(Calculadora.tecla[10])}//6
+    this.tecla[11].onclick = function(){ Calculadora.pulsa(Calculadora.tecla[11])}//resta
+    this.tecla[12].onclick = function(){ Calculadora.pulsa(Calculadora.tecla[12])}//1
+    this.tecla[13].onclick = function(){ Calculadora.pulsa(Calculadora.tecla[13])}//2
+    this.tecla[14].onclick = function(){ Calculadora.pulsa(Calculadora.tecla[14])}//3
+    this.tecla[15].onclick = function(){ Calculadora.pulsa(Calculadora.tecla[15])}//0
+    this.tecla[16].onclick = function(){ Calculadora.pulsa(Calculadora.tecla[16])}//.
+    this.tecla[17].onclick = function(){ Calculadora.pulsa(Calculadora.tecla[17])}//=
+    this.tecla[18].onclick = function(){ Calculadora.pulsa(Calculadora.tecla[18])}//suma
+  }
+};
+
+Calculadora.inicializar();
